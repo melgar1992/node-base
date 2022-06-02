e2039293f27bad8a25c9f568c8058ba264455218
miguel melgar <46229988+melgar1992@users.noreply.github.com>
2022-06-02 09:54:18 -0400

    es admin midleware
    validacion si es administrador el usuario

--- routes/user.js
+++ routes/user.js
@@ -8,9 +8,10 @@ const {
     usuariosPost,
     usuariosDelete
 } = require('../controllers/user');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { esAdminRole } = require('../middlewares/validar-roles');
 const { esRolValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -32,6 +33,7 @@ router.post('/', [
 ], usuariosPost);
 router.delete('/:id', [
     validarJWT,
+    esAdminRole,
     check('id', 'No es un ID valido').isMongoId(),
     check('id').custom(existeUsuarioPorId),
     validarCampos
