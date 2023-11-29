fa55ced1341ebdcb8b26a14cb2c883ad1c307782
miguel melgar <46229988+melgar1992@users.noreply.github.com>
2023-11-29 16:54:06 -0400

    Subir arichivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const path = require('path');
+const {v4: uuidv4} = require('uuid');
 
 
 const cargarArchivos = (req, res = response) => {
@@ -10,17 +11,31 @@ const cargarArchivos = (req, res = response) => {
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     const { archivo } = req.files;
-    uploadPath = path.join(__dirname, '../uploads/', archivo.name);
 
-    // Use the mv() method to place the file somewhere on your server
-    archivo.mv(uploadPath, (err) => {
-        if (err)
-            return res.status(500).json({
-                err
-            });
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length - 1];
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
 
-        res.json({ msg: 'File upload to' + uploadPath });
-    });
+    if (!extensionesValidas.includes(extension)) {
+        return res.status(400).json({
+            msg: `:a extension ${extension} no es permitida, ${extensionesValidas}`
+        });
+    }
+
+    const nombreTemp = uuidv4() + '.' + extension;
+    console.log(nombreTemp);
+
+    // uploadPath = path.join(__dirname, '../uploads/', nombreTemp);
+
+    // // Use the mv() method to place the file somewhere on your server
+    // archivo.mv(uploadPath, (err) => {
+    //     if (err)
+    //         return res.status(500).json({
+    //             err
+    //         });
+
+    //     res.json({ msg: 'File upload to' + uploadPath });
+    // });
 }
 
 
